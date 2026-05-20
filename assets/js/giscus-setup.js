67e06a6b80008c686dce3d194231d2d53530605c
shilpitha-03 <142985757+shilpitha-03@users.noreply.github.com>
2026-05-20 18:32:25 +0000

    Deploying to gh-pages from @ shilpitha-03/shilpitha-03.github.io@6e1acb7be0132322e0b6c9a544278b21dd6fae12 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "shilpitha-03/shilpithathokala.github.io",
+    "data-repo": "shilpitha-03/shilpitha-03.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
